31df054f4986b67a2eddb9031ee90b961bca3542
Mikhail Isaev <isaev.mihael@gmail.com>
2024-06-24 03:10:04 +0400

    🪚 Bump version

--- Sources/Webber/Commands/VersionCommand.swift
+++ Sources/Webber/Commands/VersionCommand.swift
@@ -11,7 +11,7 @@ import NIOSSL
 import WasmTransformer
 
 final class VersionCommand: Command {
-    static var currentVersion = "1.8.0"
+    static var currentVersion = "1.8.1"
     
     struct Signature: CommandSignature {
         init() {}
